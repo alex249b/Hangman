18b06d5585675804fac7fdc4336ffefcd2c00be5
Alex <138160205+alex249b@users.noreply.github.com>
2024-02-15 16:15:28 +0100

    Update hangman.py

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -20,12 +20,12 @@ def hangman():
 
     lives = 7
 
-    # getting user input
+
     while len(word_letters) > 0 and lives > 0:
         
         print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-        # what current word is (ie W - R D)
+
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print(lives_visual_dict[lives])
         print('Current word: ', ' '.join(word_list))
@@ -38,7 +38,7 @@ def hangman():
                 print('')
 
             else:
-                lives = lives - 1  # takes away a life if wrong
+                lives = lives - 1 
                 print('\nYour letter,', user_letter, 'is not in the word.')
 
         elif user_letter in used_letters:
@@ -47,7 +47,7 @@ def hangman():
         else:
             print('\nThat is not a valid letter.')
 
-    # gets here when len(word_letters) == 0 OR when lives == 0
+
     if lives == 0:
         print(lives_visual_dict[lives])
         print('You died, sorry. The word was', word)
